f74d8eff80c79da43855cd785e098b6aa9bc68c6
Willians-Hurtado <willianshurtado5@gmail.com>
2025-02-13 10:38:33 +0100

    Js-snack-1

--- Js-snack-1/main.js
+++ Js-snack-1/main.js
@@ -1,3 +1,12 @@
 /* L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore. */
 
-let userNumber = prompt("Scrivi un numero")
+let userNumber1 = prompt("Scrivi un numero");
+let userNumber2 = prompt("Scrivi un altro numero");
+
+if(userNumber1 > userNumber2){
+    console.log(userNumber1);
+    
+}else if(userNumber2 > userNumber1){
+        console.log(userNumber2);
+        
+    }
